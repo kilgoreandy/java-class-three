d83ceb86a4c0d5cb9a8a9653fd9701f56bbe978b
kilgoreandy <92450298+kilgoreandy@users.noreply.github.com>
2022-06-24 10:36:23 -0500

    Update Driver.java
    Modified JavaDOC and combined two for loops.

--- Driver.java
+++ Driver.java
@@ -1,7 +1,8 @@
-// Chicken_Fingers Driver Test
-// Create a class called Driver.java. This class will contain your main method
-// @author Andy Kilgore
-// @version 1.0.0
+/** Chicken_Fingers Driver Test
+* Create a class called Driver.java. This class will contain your main method
+* @author Andy Kilgore
+* @version 1.0.0
+*/
 
 //creating a driver to test Chicken_Fingers.java
 public class Driver{
@@ -15,7 +16,7 @@ public static void main(String[] args) {
     Chicken_Fingers chickenone = new Chicken_Fingers();
     Chicken_Fingers chickentwo = new Chicken_Fingers();
 
-    // Call all your objects set methods to assign values to your object. Assigns values to chickenone and chickentwo
+    // Call all your objects set methods to assign values to your object. Assigns values to chickenone and chickentwo.
     chickenone.setchickenName("Mandy");
     chickenone.setamount(6);
     chickenone.setweight(20.02);
@@ -30,19 +31,16 @@ public static void main(String[] args) {
     // Creating and assigning an  array to hold the chicken fingers attributes. 
     Chicken_Fingers flock[] = {chickenone, chickentwo, chickenthree, chickenfour};
 
-    // loop throught the array and call each objects display function
+    // loop throught the array and call each objects display function. After each item is displayed secretly feed the chicken. 
     for (Chicken_Fingers i : flock){
         i.display();
-    }
-
-    System.out.println("Stats after organic feeding:");
-
-    //loop through the array to call each objects eat method
-    for (Chicken_Fingers i : flock){
         i.eat(5);
     }
+
+    // Print the title 
+    System.out.println("Stats after organic feeding:");
     
-    // loop through and redisplay the updated objects stats.
+    // loop through and redisplay the updated objects stats since the chicken has ate.
     for (Chicken_Fingers i : flock){
         i.display();
     }
